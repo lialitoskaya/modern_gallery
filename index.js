58554fff4fa3a4a464047f4d8dc2214dc405b230
lialitoskaya <liyarheon@gmail.com>
2024-02-24 04:16:41 +0500

    initial project

---
+++ index.js
@@ -0,0 +1,26 @@
+let zSpacing = -1600;
+let xSpacing = 0;
+const frames = document.getElementsByClassName("frame");
+const array = Array.from(frames);
+let zvals = [];
+
+// array.forEach((f, i) => {
+//   f.setAttribute("style", `transform: translate3d(0px, 0, ${i * zSpacing}px)`);
+// });
+window.scrollTo(0, 1);
+window.document.onscroll = function () {
+  const top = document.documentElement.scrollTop;
+  let delta = top;
+  console.log(delta);
+  array.forEach((n, i) => {
+    const zval = i * zSpacing + delta * 4;
+    const xval = delta / 2;
+    const opacity = zval < -600 || zval >= 800 ? 0.2 : 1;
+    const transform = `translate3d(0px, 0, ${zval}px)`;
+    frames[i].setAttribute(
+      "style",
+      `transform: ${transform}; opacity: ${opacity}`
+    );
+  });
+  zvals = [];
+};
